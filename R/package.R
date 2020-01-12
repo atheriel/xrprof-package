5bd965f08ed5ad4756f9c41c544c92ef086a90e5
Aaron Jacobs <atheriel@gmail.com>
2020-01-12 11:56:53 -0500

    Adds enable_no_sudo() for manually enabling cap_sys_ptrace.

--- R/package.R
+++ R/package.R
@@ -46,6 +46,21 @@ xrprof <- function(file, pid = Sys.getpid(), frequency = 1, duration = 3600) {
   p
 }
 
+#' @rdname xrprof
+#' @export
+enable_no_sudo <- function() {
+  bin <- xrprof_bin()
+  pw <- askpass::askpass("A password is required for sudo:")
+  p <- processx::process$new(
+    "sudo", args = c("-S", "setcap", "cap_sys_ptrace=eip", bin),
+    stdin = "|", stdout = "|", stderr = "|", supervise = TRUE
+  )
+  p$write_input(pw)
+  p$write_input("\n")
+  p$wait(timeout = 3)
+  invisible(p)
+}
+
 xrprof_bin <- function() {
   sys <- Sys.which("xrprof")
   vendored <- system.file("vendor", "xrprof", package = "xrprof")
@@ -63,6 +78,7 @@ needs_sudo <- function(bin) {
   if (length(out$stdout) > 0 && grepl("cap_sys_ptrace", out$stdout)) {
     FALSE
   } else {
+    message("sudo is currently required to use xrprof. Try using enable_no_sudo() to fix this.")
     TRUE
   }
 }
